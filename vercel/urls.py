34e40775476c3ea793c1e52515d566f9f54f5e29
seabeePraveen <cbsrirangapraveen.2004@gmail.com>
2024-03-18 21:18:18 +0530

    deployment app removed

--- vercel/urls.py
+++ vercel/urls.py
@@ -20,6 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('upload/',include('upload.urls')),
-    path('deployment/',include('deployment.urls')),
     path('',include('server.urls')),
 ]
