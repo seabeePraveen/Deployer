10b790b260fe0adb21ed2a6c6dd0375fa33159f5
seabeePraveen <cbsrirangapraveen.2004@gmail.com>
2024-03-21 21:33:25 +0530

    small UI changes

--- frontend/src/components/Deploy/Deploy.jsx
+++ frontend/src/components/Deploy/Deploy.jsx
@@ -1,4 +1,6 @@
 import { useLocation } from 'react-router-dom';
+import Header from '../Header/Header.jsx';
+import styles from './Deploy.module.css';
 
 function Deploy() {
     const location = useLocation();
@@ -6,7 +8,13 @@ function Deploy() {
     
     
     return (
-        <div></div>
+        <div>
+            <Header />
+            <div className={styles.main_container}>
+                <input type="text" name='BaseDir' value='./' />
+
+            </div>
+        </div>
     );
 }
 
